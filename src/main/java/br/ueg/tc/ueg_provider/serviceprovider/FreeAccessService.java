9f0c399740f5d084f8044822fda05473ce9f0504
S4R4GDL <saragdl7321@gmail.com>
2025-06-01 22:34:27 -0300

    Ajustando Configuração do swagger e ajustando endpoint do intent

--- src/main/java/br/ueg/tc/ueg_provider/serviceprovider/FreeAccessService.java
+++ src/main/java/br/ueg/tc/ueg_provider/serviceprovider/FreeAccessService.java
@@ -1,5 +1,6 @@
 package br.ueg.tc.ueg_provider.serviceprovider;
 
+import br.ueg.tc.pipa_integrator.annotations.ActivationPhrases;
 import br.ueg.tc.pipa_integrator.enums.WeekDay;
 import br.ueg.tc.pipa_integrator.exceptions.BusinessException;
 import br.ueg.tc.pipa_integrator.institutions.info.IUserData;
@@ -16,7 +17,7 @@ public class FreeAccessService implements IServiceProvider {
 
     @Override
     public List<String> getValidPersonas() {
-        return List.of("ANONIMOUS");
+        return List.of("Convidado");
     }
 
     @Override
@@ -29,11 +30,10 @@ public class FreeAccessService implements IServiceProvider {
         return Boolean.FALSE;
     }
 
-    public String getScheduleSByWeek(WeekDay weekDay) {
-        return weekDay.toString();
-    }
-
-    public String getScheduleSByDay(Date date) {
-        return date.toString();
+    @ActivationPhrases({"Me conte a história da UEG", "Criação da UEG", "Como foi feita a UEG"})
+    public String getUEGHistory(){
+        return "A ideia de estabelecer uma universidade estadual em Goiás remonta à década de 1940. Nesse período, conforme documentos do Conselho Estadual de Educação, já se observava um movimento nessa direção. Contudo, o projeto não se concretizou naquele momento, sendo retomado só em 1987 pelo então governador Henrique Santillo. Ele criou uma comissão com o propósito de conduzir estudos preliminares para a criação e implantação da Universidade Estadual de Anápolis, o que efetivamente ocorreu em 1990. Naquele ano, a Faculdade de Ciências Econômicas de Anápolis (Facea), fundada em 1961, foi convertida na Universidade Estadual de Anápolis (Uniana).\n" +
+                "\n" +
+                "No ano de 1999, A Universidade Estadual de Goiás (UEG) teve sua origem na transformação da Uniana e na incorporação de outras 14 instituições de ensino superior, por meio da Lei 13.456. Sua sede foi estabelecida na cidade de Anápolis. A UEG nasceu com o objetivo central desenvolver um programa específico voltado para os trabalhadores da educação do Estado, oferecendo-lhes formação de nível superior. Isso se justificava pela constatação de que apenas 32% dos professores possuíam graduação na época. Mas a UEG não se limitou a formar professores e hoje oferta cursos em todas as áreas do conhecimento. Atualmente, a instituição oferece licenciaturas, bacharelados, cursos superiores de tecnologias, além de pós-graduações lato e stricto sensu.";
     }
 }
