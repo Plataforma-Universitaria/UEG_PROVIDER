7efde6c87691624bb24bf2f3542edec900063ac6
S4R4GDL <saragdl7321@gmail.com>
2025-06-09 21:11:14 -0300

    Ajustando listagem das instituições

--- src/main/java/br/ueg/tc/ueg_provider/UEGProvider.java
+++ src/main/java/br/ueg/tc/ueg_provider/UEGProvider.java
@@ -33,6 +33,7 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 @Getter
 public class UEGProvider implements IBaseInstitutionProvider, UEGEndpoint {
 
