f34319dff896cf4f06eeccf8c193b48c9e661727
S4R4GDL <saragdl7321@gmail.com>
2025-07-29 22:18:32 -0300

    ADD-Contatos da instituicao em FreeAccessService.java + UPDATE-Ajustando formato do método de história da ueg em FreeAccessService.java

--- src/main/java/br/ueg/tc/ueg_provider/serviceprovider/FreeAccessService.java
+++ src/main/java/br/ueg/tc/ueg_provider/serviceprovider/FreeAccessService.java
@@ -4,26 +4,74 @@ import br.ueg.tc.pipa_integrator.annotations.ServiceProviderClass;
 import br.ueg.tc.pipa_integrator.annotations.ServiceProviderMethod;
 import br.ueg.tc.pipa_integrator.interfaces.platform.IUser;
 
+import static br.ueg.tc.ueg_provider.UEGEndpoint.UEG_CONTATOS;
+
 @ServiceProviderClass(personas = {"Convidado", "Aluno", "Professor"})
 public class FreeAccessService extends InstitutionService {
 
     public FreeAccessService() {
         super();
     }
-    public FreeAccessService(IUser user){
+
+    public FreeAccessService(IUser user) {
         super(user);
     }
 
-    @ServiceProviderMethod(activationPhrases = {"Me conte a história da UEG", "Criação da UEG", "Como foi feita a UEG"})
-    public String getUEGHistory(){
-        return "A ideia de estabelecer uma universidade estadual em Goiás remonta à década de 1940. Nesse período, conforme documentos do Conselho Estadual de Educação, já se observava um movimento nessa direção. Contudo, o projeto não se concretizou naquele momento, sendo retomado só em 1987 pelo então governador Henrique Santillo. Ele criou uma comissão com o propósito de conduzir estudos preliminares para a criação e implantação da Universidade Estadual de Anápolis, o que efetivamente ocorreu em 1990. Naquele ano, a Faculdade de Ciências Econômicas de Anápolis (Facea), fundada em 1961, foi convertida na Universidade Estadual de Anápolis (Uniana).\n" +
-                "\n" +
-                "No ano de 1999, A Universidade Estadual de Goiás (UEG) teve sua origem na transformação da Uniana e na incorporação de outras 14 instituições de ensino superior, por meio da Lei 13.456. Sua sede foi estabelecida na cidade de Anápolis. A UEG nasceu com o objetivo central desenvolver um programa específico voltado para os trabalhadores da educação do Estado, oferecendo-lhes formação de nível superior. Isso se justificava pela constatação de que apenas 32% dos professores possuíam graduação na época. Mas a UEG não se limitou a formar professores e hoje oferta cursos em todas as áreas do conhecimento. Atualmente, a instituição oferece licenciaturas, bacharelados, cursos superiores de tecnologias, além de pós-graduações lato e stricto sensu.";
+    @ServiceProviderMethod(activationPhrases = {
+            "Me conte a história da UEG",
+            "Criação da UEG",
+            "Como foi feita a UEG"
+    })
+    public String getUEGHistory() {
+        return """
+                A criação da Universidade Estadual de Goiás (UEG) remonta à década de 1940, com registros de intenção já naquela época. 
+                No entanto, foi apenas em 1987, sob o governo de Henrique Santillo, que os estudos para sua implantação ganharam força. 
+                Em 1990, a Faculdade de Ciências Econômicas de Anápolis (Facea), fundada em 1961, foi transformada na Universidade Estadual de Anápolis (Uniana).
+
+                A UEG foi oficialmente criada em 1999 por meio da Lei nº 13.456, ao incorporar outras 14 instituições de ensino superior de Goiás.
+                Sua sede está localizada em Anápolis.
+
+                O principal objetivo da universidade era formar professores, já que apenas 32% tinham ensino superior na época. 
+                Atualmente, a UEG oferece cursos de graduação (licenciatura, bacharelado, tecnólogo) e pós-graduação (lato e stricto sensu) 
+                em diversas áreas do conhecimento.
+                """;
     }
 
-    //TODO: [FUNCIONALIDADE PENDENTE] Implementar 'getAvailableCourses' para listar os cursos oferecidos pela UEG.
-    // Utilizará um endpoint público.
-    // Adicionar @ServiceProviderMethod com frases como "quais cursos a UEG tem?", "listar cursos de graduação".
+    @ServiceProviderMethod(activationPhrases = {
+            "Contato",
+            "telefone da UEG",
+            "Email da secretaria"
+    })
+    public String getInstitutionContact() {
+        return """
+                 Contatos institucionais da UEG:
 
+                • Número geral: (62) 3328-1433
+
+                Campus Central:
+                • Endereço: Rodovia BR-153, Quadra Área, Km 99, Fazenda Barreiro do Meio, Anápolis/GO, CEP: 75132-400
+
+                 Secretaria Acadêmica Central:
+                • Responsável: Brandina Fátima Mendonça de Castro Andrade
+                • Telefone: (62) 3328-1402 / (62) 3328-1152 Ramais 9716 e 9715
+                • E-mail: gsec.central@ueg.br
+                • SEI: 20259
+
+                 Coordenação de Diplomas:
+                • Responsável: Jane Aparecida Borges Arantes
+                • Telefone: (62) 3328-1152
+                • E-mail: diploma.prg@ueg.br
+                • SEI: 16128
+
+                 Coordenação de Gestão das Secretarias Acadêmicas:
+                • Responsável: Lílian Lopes Fernandes
+                • Telefone: (62) 3328-1135
+                • E-mail: gsec.central@ueg.br
+                • SEI: 20261
+
+                 Secretaria do Campus Central:
+                • E-mail: secretaria.campuscentral@ueg.br
+
+                 Para mais informações, acesse:\s""" + UEG_CONTATOS;
+    }
 }
-
